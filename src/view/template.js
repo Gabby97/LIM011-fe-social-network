1c28c91b5988a0e0f0310c8b2426642ac84e85f4
MarayML <maraymontes@gmail.com>
2020-01-05 16:11:03 -0500

    init delete

--- src/view/template.js
+++ src/view/template.js
@@ -1,17 +1,24 @@
 export const templatePost = (userPost) => {
-    return `<article class = "display-flex-column center margin-bottom" >
-    <section class = "display-flex center">
-    <div class = "display-flex left border">
+    const container = document.createElement('div');
+    container.classList.add('container-posts');
+    container.id = userPost.id;
+    container.innerHTML = '';
+    const template =
+    `<section class = header-post>
+    <div>
     <img class="post-user-photo" src="${userPost.photoUser}">
     <h3>${userPost.nameUser}<h3>
     </div>
-    <div class = "display-flex rigth"><h1>${userPost.datePost}<h1><div>
+    <div><h1>${userPost.datePost}<h1><div>
     </section>
     <p>${userPost.contentPost}</p>
-    <img src="${userPost.imgPost}">
     <section class = "display-flex">
     <div class = "display-flex"><i class="far fa-thumbs-up margin-left"></i><i class="far fa-comments margin-left"></i></div>
-    <div class = "display-flex rigth"><i class="fas fa-edit margin-left"></i><i class="fas fa-trash-alt margin-left"></i></div>
-    </section>
-    </article>`
+    <div class = "display-flex rigth"><i class="fas fa-edit margin-left"></i><i class="fas fa-trash-alt margin-left" id="icon-delete-post"></i></div>
+    </section>`
+    container.innerHTML = template;
+    container.querySelector('#icon-delete-post').addEventListener('click', (e) => {
+        console.log('maray');
+    });
+    return container;
 }
